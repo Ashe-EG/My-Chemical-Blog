79d5477d42b1dd472db516b4527c4ee1615b3c1c
Alek Goss <AlekGoss01@Gmail.com>
2024-03-30 14:58:34 -0400

    2nd upload

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,19 +13,3 @@ const container = document.querySelector('.container');
   }
 });
 
-const submit = document.querySelector('submit');
-
-  submit.addEventListener('click', function() {
-    if ('.UserName' == '') {
-              alert('Please Enter Username');
-        return false;
-    } else if 
-      ('.Title' == '') {
-              alert ('please Enter Title');
-        return false;
-    } else if
-      ('.Comments'== '') {
-              alert ('Please Enter Comments');
-        return false;
-    }
-  });
